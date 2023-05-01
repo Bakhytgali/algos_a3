05d0f3b6624ec848c93e733e0310f5cf428562b3
Your Name <omgwtfwat@gmail.com>
2023-05-01 19:22:49 +0600

    commit

---
+++ MyArrayListQueue.java
@@ -0,0 +1,8 @@
+import java.util.EmptyStackException;
+import java.util.ArrayList;
+public class MyArrayListQueue<E> {
+    ArrayList<E> arrayList;
+    public MyArrayListQueue(){
+        arrayList = new ArrayList<E>();
+    }
+}
